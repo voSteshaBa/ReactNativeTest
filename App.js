ce37d578da1091c0d77b8252cd42f8f5d2ee16f7
voSteshaBa <email@youremail.com>
2020-12-14 13:46:27 +0100

    Todo App

--- App.js
+++ App.js
@@ -1,75 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
-var RNFS = require('react-native-fs');
-
-
-var path = RNFS.DocumentDirectoryPath + '/test.txt';
+import { StyleSheet, Text, View, FlatList} from 'react-native';
+import Header from './components/header';
+import TodoItem from './components/todoItem';
 
 export default function App() {
   
-    const clickWrite = () => {
+   const [todos, setTodos] = useState([
+     {text: 'buy coffe', key: '1'},
+     {text: 'create an app', key: '2'},
+     {text: 'play on the switch0', key: '3'}
+   ]);
 
-      RNFS.writeFile(path, 'StevoProba', 'utf8')
-          .then((success) => {
-            alert('File created');
-    })
-          .catch((err) => {
-          console.log('Error');
-    });
-  }
-
-    const clickRead = () => {
-      RNFS.readFile(path, 'utf8').then(res => {
-    // console.log('File read');
-      console.log(res);
-    })
-        .catch(err => {
-    
-        console.log('Error');
-    
-    });
-  }
-
-  const clickAddTxt = () => {
-
-    RNFS.appendFile(path, 'Novi tekst', 'utf8')
-            .then((success) => {
-                console.log('Text added');
-            })
-            .catch((err) => {
-                console.log('Error');               
-});
-}
-
-const clickDelete = () => {
-
-  RNFS.unlink(path)
-  .then(() => {
-      console.log('File deleted');
-  })
-  .catch((err) => {
-      console.log('Error');
-  });
-}
+   const pressHandler = (key) => {
+     setTodos((prevTodos) => {
+       return prevTodos.filter(todo => todo.key != key);
+     });
+   }
 
   return (
     
     <View style={styles.container}>
-       <View style={styles.buttonContainer}>
-            <Button title='Create File' onPress={clickWrite} />
-        </View>
-        <View style={styles.buttonContainer}>
-            <Button title='Read File' onPress={clickRead} />
-        </View>
-        <View style={styles.buttonContainer}>
-            <Button title='Add new text' onPress={clickAddTxt} />
-        </View>
-        <View style={styles.buttonContainer}>
-            <Button title='Delete file' onPress={clickDelete} />
-        </View> 
-        
-      <StatusBar style="auto" />
+      {/* header */}
+      <Header />
+
+        <View style={styles.content}>
+          {/* to form */}
+
+            <View style={styles.list}>
+                <FlatList
+                  data={todos}
+                  renderItem={( { item }) => (
+                  <TodoItem item={item} pressHandler={pressHandler} />
+                  )}               
+                />
+
+            </View>
+        </View>     
     </View>
   );
 }
@@ -78,11 +44,12 @@ const styles = StyleSheet.create({
  container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',  
   },
-  buttonContainer: {
-    margin: 10,
+  content: {
+    padding: 40
+  },
+  list: {
+    marginTop: 20
   }
 });
 
