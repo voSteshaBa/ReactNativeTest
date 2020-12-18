0fb7f87724d4dd1944f07398c57bd9169fdb2200
voSteshaBa <email@youremail.com>
2020-12-18 12:40:51 +0100

    Todo App - Alerts, Dismissing the keyboard

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, FlatList} from 'react-native';
+import { StyleSheet, Text, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard} from 'react-native';
 import Header from './components/header';
 import TodoItem from './components/todoItem';
+import AddTodo from './components/addTodo';
 
 export default function App() {
   
@@ -17,14 +18,34 @@ export default function App() {
      });
    }
 
+   const submitHandler = (text) => {
+
+    if(text.length > 3) {
+      setTodos((prevTodos) => {
+        return  [
+          {text: text, key: Math.random().toString() },
+          ...prevTodos
+        ];
+      });
+    } else {
+      Alert.alert('Error', 'Todo must be over 3 chars long', [
+        {text: 'Understood', onPress: () => console.log('alert closed')}
+      ])
+    }
+   
+   }
+
   return (
-    
+    <TouchableWithoutFeedback onPress={() => {
+      Keyboard.dismiss();
+      console.log('dismissed keyboard')
+    }}>   
     <View style={styles.container}>
       {/* header */}
       <Header />
 
         <View style={styles.content}>
-          {/* to form */}
+         <AddTodo submitHandler={submitHandler} />
 
             <View style={styles.list}>
                 <FlatList
@@ -37,6 +58,7 @@ export default function App() {
             </View>
         </View>     
     </View>
+    </TouchableWithoutFeedback>
   );
 }
 
